5220653d470dcb55c73ed20c15924d158f0a61a4
Akshat Sethi <masterakshata@gmail.com>
2021-05-30 20:12:54 +0530

    add user registeration, and request authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   post 'auth/login', to: 'authentication#authenticate'
+  post 'signup', to: 'users#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
