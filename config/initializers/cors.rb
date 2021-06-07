bdbeb690bcb5a00ac3276f2a7efe907efff63ca0
Akshat Sethi <masterakshata@gmail.com>
2021-06-07 06:44:15 +0530

    add frontend url to cors config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins 'https://awesome-tesla-2329da.netlify.app'
 
     resource '*',
       headers: :any,
